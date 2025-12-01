b5ebbdfe226c388b6534a423ad516356fc2e60a2
Teodora Dobreva <teodoramdobreva@gmail.com>
2025-12-02 01:06:16 +0200

    Initial project setup

---
+++ src/main/java/com/mentortheyoung/moneyflow/MoneyflowApplication.java
@@ -0,0 +1,13 @@
+package com.mentortheyoung.moneyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentortheyoung/moneyflow/MoneyflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentortheyoung.moneyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
