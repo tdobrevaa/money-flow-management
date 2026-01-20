67e83584c4413c3cd84e83c0247c6b4e3ac683fe
Teodora Dobreva <teodoramdobreva@gmail.com>
2026-01-20 19:34:13 +0200

    Add Income Service and Controller

--- src/main/java/com/mentortheyoung/moneyflow/controllers/IncomeController.java
+++ src/main/java/com/mentortheyoung/moneyflow/controllers/IncomeController.java
@@ -1,4 +1,26 @@
 package com.mentortheyoung.moneyflow.controllers;
 
+import com.mentortheyoung.moneyflow.entities.Income;
+import com.mentortheyoung.moneyflow.entities.UserPrincipal;
+import com.mentortheyoung.moneyflow.services.IncomeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class IncomeController {
+    private final IncomeService incomeService;
+
+    public IncomeController(IncomeService incomeService) {
+        this.incomeService = incomeService;
+    }
+
+    @PostMapping("/user/income")
+    public ResponseEntity<Income> addIncome(@RequestBody Income income,
+                                            @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        Income addedIncome = incomeService.saveIncome(income, userPrincipal.getUser());
+        return new ResponseEntity<>(addedIncome, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/mentortheyoung/moneyflow/services/IncomeService.java
+++ src/main/java/com/mentortheyoung/moneyflow/services/IncomeService.java
@@ -1,4 +1,28 @@
 package com.mentortheyoung.moneyflow.services;
 
+import com.mentortheyoung.moneyflow.entities.Income;
+import com.mentortheyoung.moneyflow.entities.User;
+import com.mentortheyoung.moneyflow.repositories.IncomeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class IncomeService {
+    private final IncomeRepository incomeRepository;
+
+    public IncomeService(IncomeRepository incomeRepository) {
+        this.incomeRepository = incomeRepository;
+    }
+
+    public Income saveIncome(Income income, User user) {
+        income.setUser(user);
+        if (income.getIncome() <= 0) {
+            throw new IllegalArgumentException("Income must be greater than 0!");
+        }
+
+        if (income.getTargetSavedMoney() < 0 || income.getTargetSavedMoney() > income.getIncome()) {
+            throw new IllegalArgumentException("Target saved money must be between 0 and total income!");
+        }
+
+        return incomeRepository.save(income);
+    }
 }
